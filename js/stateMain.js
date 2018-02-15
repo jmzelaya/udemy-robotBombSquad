db0379921a753dde6df33dc90baceea77ece08dd
Jamilet Zelaya <jamiletmzelaya@gmail.com>
2018-02-15 12:59:32 -0500

    added the map and tiles, and set collisions with the tiles, and the camera to follow the robot

--- js/stateMain.js
+++ js/stateMain.js
@@ -2,11 +2,27 @@ var StateMain = {
 
     preload: function () {
       game.load.spritesheet("robot", "images/main/robot.png", 80, 111, 28);
+      game.load.images("tiles", "images/tiles.png");
+      game.load.tilemap("map", "maps/map1.json", null, Phaser.Tilemap.TILED_JSON);
     },
 
     create: function () {
+        //Start Physics engine
         game.physics.startSystem(Phaser.Physics.ARCADE);
 
+        //Load map
+        this.map = game.add.tilemap("map");
+        this.map.addTilesetImage("tiles");
+
+        //"Tile Layer 1" refers to the name that Tiled gave to the map JSON file.
+        this.layer = this.map.createLater("Tile Layer 1");
+        //Resize the world to the size of the map
+        this.layer.resizeWorld();
+        //Set which tiles you want to enable collisions for
+        //in this case it's ALL of the tiles EXCEPT the bomb 💣
+        this.map.setCollisionBetween(0,24);
+
+        //Add robot and its animations
         this.robot = game.add.sprite(150, 150, "robot");
         this.robot.animations.add("idle", [0,1,2,3,4,5,6,7,8,9], 12, true);
         this.robot.animations.add("walk", [10, 11, 12, 13, 14, 15, 16, 17], 12, true);
@@ -19,10 +35,14 @@ var StateMain = {
         this.robot.body.bounce.set(0.25);
         this.robot.body.collideWorldBounds = true;
 
+        //Make the camera follow the robot!
+        game.camera.follow(this.robot);
+
 
     },
 
     update: function () {
+      game.physics.arcade.collide(this.robot, this.layer);
 
     }
 
